2ec98244710acf58f3a85d17e0558575bda3fd42
LopesLee <142916044+LopesLee@users.noreply.github.com>
2023-11-20 22:42:01 -0300

    Update SomaNVet.java

--- vetsIntroducao/SomaNVet.java
+++ vetsIntroducao/SomaNVet.java
@@ -4,21 +4,21 @@ public class SomaNVet{
     
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        final int TAM=5; 
+        final int TAM=10; 
         int a[],b[],c[],i;
         a = new int[TAM];
-        b= new int[TAM];
+        b = new int[TAM];
         c = new int[TAM];
 
 
         for (i=0; i<TAM; i++) { 
-            System.out.println("Digite o "+(i+1)+"° Valor para A");
+            System.out.println("Digite o "+(i+1)+"Â° Valor para A");
             a[i]= ler.nextInt();
         }
         	
         		System.out.println("\n"+"Agora vamos ao vetor B"+"\n");
         	
-        for (i=0; i<TAM; i++) { System.out.println("Digite o "+(i+1)+"° Valor para B");
+        for (i=0; i<TAM; i++) { System.out.println("Digite o "+(i+1)+"Â° Valor para B");
             b[i]= ler.nextInt(); 
             c[i]=a[i]+b[i];
         }
