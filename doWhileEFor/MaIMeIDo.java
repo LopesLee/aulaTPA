53c294c493ac466609b103d00513754bf29bb150
LopesLee <142916044+LopesLee@users.noreply.github.com>
2023-11-21 11:56:26 -0300

    Create MaIMeIDo.java

---
+++ doWhileEFor/MaIMeIDo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class MaIMeIDo {
+    
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int anoA, anoN, idade, i;
+        do{
+            System.out.println("Digite o ano atual:");
+            anoA=ler.nextInt();
+            System.out.println("Digite seu ano de nascimento:");
+            anoN=ler.nextInt();
+            idade=anoA-anoN;
+            if(idade>=18){
+                System.out.println("Você tem "+idade+", maior de idsde.");
+            }else{
+                System.out.println("Você tem "+idade+", menor de idade.");
+            }System.out.println("Continuar a execução, se sim digite 1, senão digite 2");
+            i=ler.nextInt();
+        }while(i==1);
+    }
+}
