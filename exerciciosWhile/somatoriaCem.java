db51369f8aff589850c645a765a7d901432d46d9
LopesLee <142916044+LopesLee@users.noreply.github.com>
2023-11-21 10:53:03 -0300

    Create somatoriaCem.java

---
+++ exerciciosWhile/somatoriaCem.java
@@ -0,0 +1,12 @@
+public class somatoriaCem{
+    public static void main(String[] args){
+        int soma=0, i=1;
+
+        while (i<=100) {
+            somatoria += i;
+            i++;
+        }
+
+        System.out.println("A soma dos números de 1 a 100 é:"+ somatoria);
+    }
+}
