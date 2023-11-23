3f9407d008914da6dd641aa99a8d98d14f0e4521
LopesLee <142916044+LopesLee@users.noreply.github.com>
2023-11-22 23:28:44 -0300

    Update V_parImparComVetorC.java

--- vetsCompl/V_parImparComVetorC.java
+++ vetsCompl/V_parImparComVetorC.java
@@ -1,28 +1,28 @@
-package exs15;
+package listaComplementar;
 
-import java.util.*;
+import java.util.Scanner;
 
-public class V_parImparComVetorC {
+public class ParOuImparVetMaMe {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 
-		// declarando vari·veis
+		// declarando vari√°veis
 		final int TAM = 10;
 		int i, a[], b[], c[];
 		a = new int[TAM];
 		b = new int[TAM];
 		c = new int[TAM];
 
-		// criando laÁos
+		// criando la√ßos
 		for (i = 0; i < TAM; i++) {
-			System.out.println("Entre com o " + (1 + i) + "o. elemento do vetor A[]:");
+			System.out.println("Entre com o " + (1 + i) + "¬∞. elemento do vetor A:");
 			a[i] = in.nextInt();
 
-			System.out.println("Entre com o " + (1 + i) + "o. elemento do vetor B[]:");
+			System.out.println("Entre com o " + (1 + i) + "¬∞. elemento do vetor B:");
 			b[i] = in.nextInt();
 
-			// atribuindo condiÁıes
+			// condi√ß√µes
 			if (a[i] > b[i]) {
 				c[i] = 1;
 				System.out.println("Maior. Vetor C recebe[ " + c[i] + " ].");
