aabb17e24a39617ce1518c32abab514d769ff213
LopesLee <142916044+LopesLee@users.noreply.github.com>
2023-11-22 23:31:15 -0300

    Update and rename V_parImparComVetorB.java to Par1ImparMenos1.java

--- vetsCompl/Par1ImparMenos1.java
+++ vetsCompl/Par1ImparMenos1.java
@@ -1,20 +1,20 @@
-package exs15;
 
-public class V_parImparComVetorB {
+
+public class Par1ImparMenos1 {
 	public static void main(String[] args) {
 
-		// declarando vari·veis
+		// declarando vari√°veis
 		final int TAM = 10;
 		int i, a[], b[];
 		a = new int[TAM];
 		b = new int[TAM];
 
-		// criando laÁos
+		// criando la√ßos
 		for (i = 0; i < TAM; i++) {
 			a[i] = i + 1;
 			System.out.println("Vetor A[ " + a[i] + " ].");
 
-			// atribuindo condiÁıes
+			// atribuindo condi√ß√µes
 			if (a[i] % 2 == 0) {
 				b[i] = 1;
 				System.out.println("Par. Vetor B[ " + b[i] + " ].");
